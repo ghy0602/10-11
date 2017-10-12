877c802663bab9e7a9b7ec5549b3de2b9c232b92
ghy0602 <458686668@qq.com>
2017-10-12 16:44:58 +0800

    down

---
+++ 1.js
@@ -0,0 +1,6 @@
+var box = document.getElementsByTagName('div');
+box.addEventListener('mousedown',down);
+function down(ev){
+	var disX = ev.clientX - this.offsetLeft;
+	var disY = ev.clientY - this.offsetTop;
+}
